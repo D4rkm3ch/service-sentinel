89d3af4612db0c472d0aacf93fdaf2d83fea30a2
D4rkm3ch <53240705+D4rkm3ch@users.noreply.github.com>
2026-07-03 22:22:58 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,12 @@ usermod -o -u "$PUID" appuser
 
 chown -R appuser:appuser /data
 
+if [ -S /var/run/docker.sock ]; then
+  DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+  if ! getent group "$DOCKER_GID" >/dev/null 2>&1; then
+    groupadd -g "$DOCKER_GID" dockersock
+  fi
+  usermod -aG "$DOCKER_GID" appuser
+fi
+
 exec gosu appuser "$@"
